1038f4bc78747574c786ad93f4ebbe3625941e80
sadafsalimii <146592774+sadafsalimii@users.noreply.github.com>
2025-04-01 20:41:47 -0400

    app.py

--- app.py
+++ app.py
@@ -79,5 +79,8 @@ def show_year_result(year):
     runner_up = match["RunnerUp"].values[0]
     return html.P(f"In {year}, {winner} won against {runner_up}.")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+import os
+
+port = int(os.environ.get("PORT", 8050))
+app.run(host="0.0.0.0", port=port, debug=True)
